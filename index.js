964f3f233ba8964196cfe40383af9d8213123517
valentin charrier <valentin.charrier@htomail.fr>
2023-08-23 22:47:41 +0200

    Trim en chainage.

--- index.js
+++ index.js
@@ -155,7 +155,9 @@ const RunVersionning = (indexFile=false) => {
   const newVersion = extractVersion(pathIndex);
   core.setOutput("version", newVersion);
   const comment = extractComment(getFileContent(pathIndex));
-  const commentNewVersion = comment.replace(/Version:\s*.*\n/, `Version: ${newVersion}\n`);
+  const commentNewVersion = comment.replace(/(Version:)(\s*)([^\n]*)\n/, (match, p1, p2, p3) => {
+    return `${p1} ${newVersion}\n`;
+  });  
   const json = commentToJSON(commentNewVersion);
   json["is_plugin"] = indexFile ?  true : false;
   // Que ce soit un fichier .php on style.css on remplace le commentaire par le nouveau
